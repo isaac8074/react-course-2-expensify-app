b8983fc05749e501a7aa7bb04a65bd924edefd1d
Jose Bustamante <josebustamante@joses-mbp.attlocal.net>
2022-02-19 12:28:15 -0600

    Commit for Authentication and firebase

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import NotFoundPage from '../../components/NotFoundPage';
+import { Header } from '../../components/Header';
 
-test('should render NotFoundPage correctly', () => {
-  const wrapper = shallow(<NotFoundPage />);
+test('should render Header correctly', () => {
+  const wrapper = shallow(<Header startLogout={() => { }} />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
 });
